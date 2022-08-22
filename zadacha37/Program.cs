723c5b15eeaaf9d3e6a1c8d34f95bc97b473c5fb
Evgeniy <evg961@mail.ru>
2022-08-22 22:06:25 +0300

    35

---
+++ zadacha37/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main (string[] args)    
+    {
+        int count = 0;
+        int[] array = new int [123];
+        for(int i = 0; i < array.Length; i++)
+        {
+            //array[i] = Convert.ToInt32(new Random().Next(123));
+            array[i] = new Random().Next(0,1000);
+            Console.Write(array[i]+ " ");
+            int min = 10;
+            int max = 99;
+            if(array[i]> min && array[i]< max)
+            {
+                count++;
+
+            }
+            
+        }
+        System.Console.WriteLine("колличество " + count);
+        
+    }
+}         
